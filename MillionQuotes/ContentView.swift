53c2057a2352e327e0e207edc3e776aef4c7294e
Fito Toledano <fito.toledano@gmail.com>
2022-04-03 23:23:06 +0200

    Initial Commit

---
+++ MillionQuotes/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MillionQuotes
+//
+//  Created by Fito Toledano on 03/04/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MillionQuotes/MillionQuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MillionQuotesApp.swift
+//  MillionQuotes
+//
+//  Created by Fito Toledano on 03/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct MillionQuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
